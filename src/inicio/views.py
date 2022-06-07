1cfde9163f64c1ddd9cc55eae8c65d8aeaa5dfbf
Triplerush <fcanal@unsa.edu.pe>
2022-06-07 18:50:23 -0500

    Templates

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def myHomeView(request,*args, **kwargs):
+    print(request.user)
+    print(args, kwargs)
+    return render(request,"home.html",{})
