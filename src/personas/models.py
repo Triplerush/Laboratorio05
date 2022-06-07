da95feb2966a03f2962b4dda7acc7a5e7607834d
Triplerush <fcanal@unsa.edu.pe>
2022-06-07 18:03:27 -0500

    Modificando el tipo de dato en Persona

--- src/personas/models.py
+++ src/personas/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-  nombres = models.TextField()
-  apellidos = models.TextField()
-  edad = models.TextField()
+  nombres = models.CharField(max_length=100)
+  apellidos = models.CharField(max_length=100)
+  edad = models.IntegerField()
+  donador =  models.BooleanField(default=False)
 
