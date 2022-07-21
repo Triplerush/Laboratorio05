507744a6ce50e5fddc9f8188c002ed2268d027e1
Triplerush <fcanal@unsa.edu.pe>
2022-07-20 23:01:11 -0500

    Utilizando widgets en el campo del nombre

--- src/personas/forms.py
+++ src/personas/forms.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from django import forms
 from .models import Persona
 
@@ -13,7 +14,17 @@ class PersonaForm(forms.ModelForm):
         ]
 
 class RawPersonForm(forms.Form):
-  nombres = forms.CharField()
+  nombres = forms.CharField(
+    widget = forms.Textarea(
+      attrs = {
+        'placeholder' : 'Sólo tu nombre, por favor',
+        'id': 'nombreID',
+        'class' : 'special',
+        'cols' : '10',
+      }
+    )
+  )
+
   apellidos = forms.CharField()
-  edad = forms.IntegerField()
+  edad = forms.IntegerField(initial=25)
   donador = forms.BooleanField()
